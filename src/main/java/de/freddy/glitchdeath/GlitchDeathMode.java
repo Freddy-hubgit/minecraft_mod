f066c423bfb6560becf60771b62dcc78eb1a3cbd
Freddy-hubgit <freddypauw@gmail.com>
2026-09-12 16:17:15 +0200

    Update GlitchDeathMode.java

--- src/main/java/de/freddy/glitchdeath/GlitchDeathMode.java
+++ src/main/java/de/freddy/glitchdeath/GlitchDeathMode.java
@@ -1,16 +1,28 @@
 package de.freddy.glitchdeath;
 
 public enum GlitchDeathMode {
+
     NAME,
     GLITCH,
     RANDOM;
 
-    public static GlitchDeathMode fromString(String value) {
+    public static GlitchDeathMode fromString(
+            String value
+    ) {
+
         return switch (value.toLowerCase()) {
-            case "name", "normal" -> NAME;
-            case "glitch" -> GLITCH;
-            case "random" -> RANDOM;
-            default -> null;
+
+            case "name", "normal" ->
+                    NAME;
+
+            case "glitch" ->
+                    GLITCH;
+
+            case "random" ->
+                    RANDOM;
+
+            default ->
+                    null;
         };
     }
 }
